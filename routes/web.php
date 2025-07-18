053b554501012775e9f641271a9dfa61cabcf020
oshu <99813212+oshudev@users.noreply.github.com>
2025-07-18 10:37:37 +0800

    fix(auth): add remember token to users table and rename logout route method

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 
-    Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+    Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
 });
 
 Route::middleware('guest')->group(function () {
